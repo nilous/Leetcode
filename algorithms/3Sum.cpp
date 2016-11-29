cd594e6b243fd70b32d79cded9b80e8494f27182
Feng Chen <chenf@outlook.com>
2016-11-29 10:53:12 +0800

    15. add another solution for 3Sum

--- algorithms/3Sum.cpp
+++ algorithms/3Sum.cpp
@@ -16,6 +16,7 @@ The solution set must not contain duplicate triplets.
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <iterator>  // for std::back_inserter
 using std::vector;
 
 /**
@@ -77,6 +78,108 @@ vector<vector<int>> threeSum2(vector<int> &num) {
     return result;
 }
 
+/**
+ 在twoSum的基础上进行。取出一个数x，对剩下的数求(target-x)的two sum解。
+*/
+
+// 这个解法会TLE...
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int> &num) {
+        vector<vector<int>> r;
+
+        std::sort(num.begin(), num.end());
+        for (auto i = 0; i < num.size(); ++i) {
+            auto pr = twoSum(num, 0 - num[i], i);
+            std::copy(pr.begin(), pr.end(), std::back_inserter(r));
+        }
+
+        std::sort(r.begin(), r.end());
+        r.resize(std::distance(r.begin(), std::unique(r.begin(), r.end())));
+        return r;
+    }
+
+private:
+    // NOTE!: 可能有多组解，所以是个vector的vector
+    vector<vector<int>> twoSum(vector<int> &sorted_num, int target, int skip) {
+        vector<vector<int>> r;
+
+        int b = 0;
+        int e = sorted_num.size() - 1;
+
+        while (1) {
+            if (b == skip) ++b;
+            if (e == skip) --e;
+            if (b >= e) break;
+
+            int sum = sorted_num[b] + sorted_num[e];
+            if (sum == target) { // 不要break，可能有更多解
+                vector<int> t{ sorted_num[b], sorted_num[e], sorted_num[skip] };
+                std::sort(t.begin(), t.end());
+                r.emplace_back(t);
+                ++b; // <- 随意移动一个游标就可以
+            } else if (sum < target)
+                ++b;
+            else --e;
+        }
+
+        return r;
+    }
+};
+
+// 在上面解法上加以改进，不会TLE了。本质上还是个O(n^2)，实际跑略慢于解法2
+class Solution1 {
+public:
+    vector<vector<int>> threeSum(vector<int> &num) {
+        vector<vector<int>> r;
+
+        std::sort(num.begin(), num.end());
+
+        for (auto i = 0; i < num.size(); ++i) {
+            if (i != 0 && num[i-1] == num[i]) continue;  // 跳过相同的数
+
+            auto pr = twoSum(num, 0 - num[i], i);
+            if (!pr.empty())
+                std::copy(pr.begin(), pr.end(), std::back_inserter(r));
+        }
+
+        return r;
+    }
+
+private:
+    // NOTE!: 可能有多组解，所以是个vector的vector
+    vector<vector<int>> twoSum(vector<int> &sorted_num, int target, int skip) {
+        vector<vector<int>> r;
+
+        int b = skip+1;
+        int e = sorted_num.size() - 1;
+
+        while (b < e) {
+            int sum = sorted_num[b] + sorted_num[e];
+            if (sum == target) { // 不要break，可能有更多解
+                vector<int> t{ sorted_num[skip], sorted_num[b], sorted_num[e] };
+                r.emplace_back(t);
+
+                // 随意移动一个游标就可以
+                do {
+                    ++b;
+                } while (sorted_num[b] == sorted_num[b-1]);
+            } else if (sum < target) {
+                do {  // 跳过相同的数
+                    ++b;
+                } while (sorted_num[b] == sorted_num[b-1]);
+            } else {
+                do {  // 跳过相同的数
+                    --e;
+                } while (sorted_num[e] == sorted_num[e+1]);
+            }
+        }
+
+        return r;
+    }
+};
+
 struct TestCase {
     vector<int> nums;
     vector<vector<int>> expected;
@@ -88,7 +191,7 @@ std::vector<TestCase> cases {
     { { 1, 2 }, {} },
     { { 1, 2, 3 }, {} },
     { { 1, 2, -3 }, { { -3, 1, 2 } } },
-    { { -1, 0, 1, 2, -1, 4 }, { { -1, 0, 1 }, { -1, -1, 2 } } } 
+    { { -1, 0, 1, 2, -1, 4 }, { { -1, 0, 1 }, { -1, -1, 2 } } }
 };
 
 #include <assert.h>
@@ -103,7 +206,17 @@ int main(int argc, char **argv) {
         std::sort(c.expected.begin(), c.expected.end());
         assert(r == c.expected);
     }
-    
+
+    vector<int> v{ -1, 0, 1, 2, -1, -4 };
+    //vector<int> v{ 0, 0, 0, 0 };
+    auto r = Solution1().threeSum(v);
+    for (auto vv : r) {
+        for (auto i : vv) {
+            std::cout << i << ' ';
+        }
+        std::cout << '\n';
+    }
+
 
     return 0;
 }
